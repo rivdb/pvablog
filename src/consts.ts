7ada13b0b7d313394a11ad8e89eea0885f49f3ff
rivers-sh <dreamhoppin@gmail.com>
2024-12-05 01:58:10 -0500

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,39 @@
+/**
+ * Site metadata that is used across the site.
+ *
+ * A few of these are not used yet, and are subject to change, example of this is Author.
+ */
+export const SITE_METADATA = {
+  theme: "system", // Options: system, light, dark
+  siteUrl: "https://tasb.yon.fun/",
+  siteRepo: "https://github.com/wanoo21/tailwind-astro-starting-blog",
+  robots: "index, follow", // Options: index, noindex, follow, nofollow
+}
+
+
+/**
+ * Navigation items.
+ If title is not found in the translation file, it will be used as is.
+ example: if title is "nav.home", and translation file does not have "nav.home", it will be displayed as "nav.home"
+
+ You should add translations for these in src/i18n/ui.ts or use as is.
+ */
+export const NAVIGATION = [
+  { href: "/", title: "nav.home" },
+  { href: "/blog", title: "nav.blog" },
+  { href: "/tags", title: "nav.tags" },
+  { href: "/about", title: "nav.about" },
+] as const;
+
+export const POST_METADATA = {
+  defaultLayout: "column", // Default layout for blog posts, options: simple and column
+  showFullWidthCover: false, // Show full width cover image in blog post
+  showCover: true, // Show cover image in blog post
+  showTags: true, // Show tags in blog post, TODO: Add support for hiding tags
+  showDate: true, // Show date in blog post, TODO: Add support for hiding date
+  showSummary: true, // Show summary in blog post
+  showAuthors: true, // Show authors in blog post, TODO: Add support for hiding authors
+  showRelatedPosts: true, // Show related posts in blog post, TODO: Add support for hiding related posts
+  showTableOfContents: true, // Show table of contents in blog post
+  showShareButtons: 'both', // Show share buttons in blog post, options: top, bottom, both, none
+};
