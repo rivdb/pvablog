b73d021ea79be73adbbb12ad14e7787256bc21e6
rivers-sh <dreamhoppin@gmail.com>
2024-12-05 14:33:35 -0500

    fixed: astro.config.mjs and social import for authorlayout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  site: 'https://rivdb.github.io'
-  base: '/pvablog'
+  site: 'https://rivdb.github.io',
+  base: '/pvablog',
   integrations: [mdx(), sitemap(), tailwind(), solidJs(), metaTags(), robotsTxt()]
 });
